eb7e539a7f28228a0fd6a090a9c55a3ff306ef16
Srikanth.n <srikanth@1cell.ai>
2025-12-02 21:54:18 +0530

    Landing and components added

---
+++ components/navigation/VSpace.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const VSpace = ({ gap }: any) => {
+  return <View style={{ height: gap }}></View>;
+};
+export default VSpace;
