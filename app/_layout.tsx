6493330caebcd0ae85689b6cfb5500614e90fd16
Srikanth.n <srikanth.nandiraju@1cell.ai>
2025-12-10 18:46:15 +0530

    Font fix and test flight build added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+import { GestureHandlerRootView, TextInput } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Toast from 'react-native-toast-message';
 import { AuthProvider } from '@/context/AuthContext';
@@ -9,6 +16,20 @@ import NetworkChecker from '@/hooks/NetworkChecker';
 import '../global.css';
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  });
+  if (!loaded) return null;
+
+  if (Text.defaultProps == null) Text.defaultProps = {};
+  Text.defaultProps.style = { fontFamily: 'Poppins_400Regular' };
+
+  if (TextInput.defaultProps == null) TextInput.defaultProps = {};
+  TextInput.defaultProps.style = { fontFamily: 'Poppins_400Regular' };
+
   return (
     <SafeAreaProvider>
       <GestureHandlerRootView>
