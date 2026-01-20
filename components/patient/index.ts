8d1f803d669305ad3fd3a518703d4638018c2c09
Ankitsingh Bisht <ankitsingh.bisht@indxtechnology.com>
2026-01-20 12:11:47 +0530

    implement notification permission modal and refactor push notification logic

--- components/patient/index.ts
+++ components/patient/index.ts
@@ -3,3 +3,4 @@ export { PatientHeader } from './PatientHeader';
 export { FilterModal } from './FilterModal';
 export { EmptyState } from './EmptyState';
 export { LoadingIndicator } from './LoadingIndicator';
+export { default as NotificationPermissionModal } from './NotificationPermissionModal';
